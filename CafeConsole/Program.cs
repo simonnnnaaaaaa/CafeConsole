f54fb6183309206d4ef2e1e475fd94b347709416
Simona Boicu <Simona.Boicu@endava.com>
2025-12-22 13:53:24 +0200

    Implemented state pattern

--- CafeConsole/Program.cs
+++ CafeConsole/Program.cs
@@ -7,6 +7,7 @@ using Cafe.Domain.Preparation;
 using Cafe.Domain.Pricing;
 using Cafe.Infrastructure.Factories;
 using Cafe.Infrastructure.Observers;
+using CafeConsole.States;
 
 
 namespace CafeConsole
@@ -16,9 +17,7 @@ namespace CafeConsole
 
         static void Main(string[] args)
         {
-            //factory
-            IBeverageFactory beverageFactory = new BeverageFactory();
-
+            
             //observers
             var analytics = new InMemoryOrderAnalytics();
             var logger = new ConsoleOrderLogger();
@@ -31,61 +30,15 @@ namespace CafeConsole
             //service
             IOrderService orderService = new OrderService(eventPublisher);
 
+            //state
+            var kiosk = new KioskContext(orderService, analytics);
+
             //////////////////////////////////////////////////////////////
 
-            bool running = true;
-
-            while (running)
+            while (true)
             {
-                Console.Clear();
-                Console.WriteLine("=== Cafe Console ===");
-                Console.WriteLine();
-
-                var beverage = MenuHelper.ChooseBaseBeverage(beverageFactory);
-                beverage = MenuHelper.ChooseAddOns(beverage);
-
-                PreparationHelper.SimulatePreparation(beverage);
-
-                IPricingStrategy pricingStrategy = MenuHelper.ChoosePricingStrategy();
-                var resultOrder = orderService.PlaceOrder(beverage, pricingStrategy);
-
-                Console.WriteLine("=== Receipt ===");
-                MenuHelper.PrintReceipt(resultOrder);
-                Console.WriteLine("===============");
-
-                Console.WriteLine();
-                Console.WriteLine("What would you like to do next?");
-                Console.WriteLine("1) Place another order");
-                Console.WriteLine("0) Exit");
-                Console.Write("Your choice: ");
-
-                var next = Console.ReadLine();
-
-                switch (next)
-                {
-                    case "1":
-                        break;
-
-                    case "0":
-                        running = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Invalid option. Returning to main menu...");
-                        Thread.Sleep(1000);
-                        break;
-                }
+                kiosk.Run();
             }
-
-            Console.WriteLine();
-            Console.WriteLine("=== Session analytics ===");
-            Console.WriteLine($"Total orders: {analytics.TotalOrders}");
-            Console.WriteLine($"Total revenue: {analytics.TotalRevenue:F2}");
-
-
-            Console.WriteLine();
-            Console.WriteLine("Press any key to exit!");
-            Console.ReadKey();
         }
 
         
---
+++ CafeConsole/States/PrintingState.cs
@@ -0,0 +1,26 @@
+﻿namespace CafeConsole.States
+{
+    internal class PrintingState : IKioskState
+    {
+        public void Handle(KioskContext context)
+        {
+            //place the order
+            var resultOrder = context.OrderService.PlaceOrder(
+                context.Beverage!,
+                context.PricingStrategy!
+            );
+
+            context.OrderResult = resultOrder;
+
+            Console.WriteLine("=== Receipt ===");
+            MenuHelper.PrintReceipt(resultOrder);
+            Console.WriteLine("===============");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to return to main menu...");
+            Console.ReadKey();
+
+            context.State = new IdleState();
+        }
+    }
+}
